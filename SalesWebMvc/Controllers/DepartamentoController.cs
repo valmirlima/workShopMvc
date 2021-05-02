8f98cec99187fe3ec980d23dbb3b6c34f63d67d4
valmir <vallfimm@gmail.com>
2021-05-02 15:10:00 -0300

    Firste controller deparment

---
+++ SalesWebMvc/Controllers/DepartamentoController.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using SalesWebMvc.Models;
+
+namespace SalesWebMvc.Controllers
+{
+    public class DepartamentoController : Controller
+    {
+        public IActionResult Index()
+        {
+            List<Departamento> list = new List<Departamento>();
+            list.Add(new Departamento{Id = 1, Name = "Eletronico" });
+            list.Add(new Departamento { Id = 2, Name = "Fashion" });
+            return View(list);
+        }
+    }
+}
--- SalesWebMvc/Controllers/HomeController.cs
+++ SalesWebMvc/Controllers/HomeController.cs
@@ -4,9 +4,9 @@ using System.Diagnostics;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
-using SalesWebMvc.Models;
+using SalesWebMvc.Models.ViewModels;
 
-namespace SalesWebMvc.Controllers
+namespace SalesWebMvc.Controllers.ViewModels
 {
     public class HomeController : Controller
     {
